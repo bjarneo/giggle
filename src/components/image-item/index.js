46ac6eb1666fe529a559aac5a0ff888879fad9a0
bjarneo <bjarne.overli@vg.no>
2016-09-18 19:09:32 +0200

    lazy load images

--- src/components/image-item/index.js
+++ src/components/image-item/index.js
@@ -1,13 +1,16 @@
 import React from 'react';
 import { Link } from 'react-router';
+import LazyLoad from 'react-lazyload';
 import styles from './style';
 
 export default ({ link, title, cover, id }) => (
     <div className={ styles.item }>
         <Link to={link}>
-            <img src={`//i.imgur.com/${cover || id}b.jpg`}
-              alt={title}
-            />
+            <LazyLoad height={180} width={180} offset={100}>
+                <img src={`//i.imgur.com/${cover || id}b.jpg`}
+                  alt={title}
+                />
+            </LazyLoad>
         </Link>
 
         <div className={styles.overlay}>{title}</div>
