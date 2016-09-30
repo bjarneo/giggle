234856cc98d308cb79a462559d606928ec7e6d9b
Bjarne Oeverli <bjarne.oeverli@gmail.com>
2016-09-30 13:59:26 +0200

    add icons

--- src/components/image-item/index.js
+++ src/components/image-item/index.js
@@ -3,7 +3,7 @@ import { Link } from 'react-router';
 import LazyLoad from 'react-lazyload';
 import styles from './style';
 
-export default ({ link, title, cover, id }) => (
+export default ({ link, title, cover, id, ups, downs, comment_count }) => (
     <div className={ styles.item }>
         <Link to={link}>
             <LazyLoad height={180} width={180} offset={100}>
@@ -14,5 +14,20 @@ export default ({ link, title, cover, id }) => (
         </Link>
 
         <div className={styles.overlay}>{title}</div>
+
+        <div className={styles['icons-wrapper']}>
+            <div>
+                <i className={"material-icons " + styles.up}>thumb_up</i>
+                <span>{ups}</span>
+            </div>
+            <div>
+                <i className={"material-icons " + styles.down}>thumb_down</i>
+                <span>{downs}</span>
+            </div>
+            <div>
+                <i className={"material-icons " + styles.comments}>comment</i>
+                <span>{comment_count}</span>
+            </div>
+        </div>
     </div>
 );
