1a93a131a2b3b5a0219d06378cb04dd2caeb8e72
bjarneo <bjarne.overli@vg.no>
2016-11-06 21:27:22 +0100

    remove react router and use a tags

--- src/components/image-item/index.js
+++ src/components/image-item/index.js
@@ -1,18 +1,17 @@
 import classnames from 'classnames';
 import React from 'react';
-import { Link } from 'react-router';
 import LazyLoad from 'react-lazyload';
 import styles from './style';
 
 export default ({ link, title, cover, id, ups, downs, comment_count }) => (
     <div className={ styles.item }>
-        <Link to={link}>
+        <a href={link}>
             <LazyLoad height={180} width={180} offset={100}>
                 <img src={`//i.imgur.com/${cover || id}b.jpg`}
                   alt={title}
                 />
             </LazyLoad>
-        </Link>
+        </a>
 
         <div className={styles.overlay}>{title}</div>
 
